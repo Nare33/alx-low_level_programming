bf251f392a12d0e108a3b7488ba8ffa122afd004
Nare33 <ulendaphaho@gmail.com>
2023-10-10 13:58:40 -0700

    Times table

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,17 @@
 
 void jack_bauer(void)
 {
-	int i' j;
+        int i' j;
 
-	for (i = 0 ; i < 24 ; i++)
-	{
-		for (j = 0 ; j < 60 ; j++)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar('i');
-			_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-	}
+        for (i = 0 ; i < 24 ; i++)
+        {
+                for (j = 0 ; j < 60 ; j++)
+                {
+                        _putchar(i / 10 + '0');
+                        _putchar(i % 10 + '0');
+                        _putchar('i');
+                        _putchar(j / 10 + '0');
+                        _putchar(j % 10 + '0');
+                }
+        }
 }
